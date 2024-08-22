848bcd9e0aa320f6b756400987ec1bb19f403385
mac <habeebmustapha96@gmail.com>
2024-08-22 15:17:45 +0100

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,7 +18,10 @@ const App = () => {
         <Navbar />
 
           <Routes>
-            <Route path='/' element={<Home />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/create-campaign" element={<CreateCampaign />} />
+            <Route path="/campaign-details/:id" element={<CampaignDetails />} />
           </Routes>
       </div>
   </div>
